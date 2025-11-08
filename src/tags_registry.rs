493bd8a68bd20fb6e77ac45613a14068c5a4c5d8
Wolf McNally <wolf@wolfmcnally.com>
2025-11-07 18:01:23 -0800

    Remove 102 nan-bstr

--- src/tags_registry.rs
+++ src/tags_registry.rs
@@ -19,8 +19,6 @@ pub use dcbor::prelude::*;
 const_cbor_tag!(32, URI, "url");
 const_cbor_tag!(37, UUID, "uuid");
 
-const_cbor_tag!(102, NAN_BSTR, "nan-bstr");
-
 // A previous version of the Envelope spec used tag #6.24 ("Encoded CBOR Item")
 // as the header for the Envelope `leaf` case. Unfortunately, this was not a
 // correct use of the tag, as the contents of #6.24 (RFC8949 §3.4.5.1) MUST
